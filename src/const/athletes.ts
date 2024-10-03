90e8864a38992d696144fbcf9e230fbd7048b775
Mario Chico <chico.developer@hotmail.com>
2024-10-02 19:58:57 -0600

    Cambiando el color de bg, agregando logo del evento. Empezando componente de atletas

---
+++ src/const/athletes.ts
@@ -0,0 +1,32 @@
+export const ATHLETES = [
+  {
+    id: "mario",
+    name: "Mario",
+    weight: 65,
+    category: "streelifting",
+  },
+  {
+    id: "luigi",
+    name: "Luigi",
+    weight: 60,
+    category: "streelifting",
+  },
+  {
+    id: "yoshi",
+    name: "Yoshi",
+    weight: 55,
+    category: "streelifting",
+  },
+  {
+    id: "peach",
+    name: "Peach",
+    weight: 50,
+    category: "power freestyle",
+  },
+  {
+    id: "bowser",
+    name: "Bowser",
+    weight: 45,
+    category: "power freestyle",
+  },
+]
