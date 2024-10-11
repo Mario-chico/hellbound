d0223489e6881ca63ea4721963d2cea27c5faba2
Mario Chico <chico.developer@hotmail.com>
2024-10-10 20:20:21 -0600

    Cambio de id en atletas

--- src/const/athletes.ts
+++ src/const/athletes.ts
@@ -1,46 +1,46 @@
 import { type Athletes } from "@/types/Athletes";
 export const ATHLETES: Athletes[] = [
   {
-    id: "mario",
+    id: "yael",
     name: "Mario Chico",
     weight: 65,
     category: "streelifting",
     gender: "masculino"
   },
   {
-    id: "alfredo",
+    id: "javier",
     name: "Alfredo",
     weight: 60,
     category: "streelifting",
     gender: "masculino"
   },
   {
-    id: "jony",
+    id: "cesar",
     name: "Jony Workout",
     weight: 55,
     category: "streelifting",
     gender: "masculino"
   },
   {
-    id: "vale",
+    id: "lena",
     name: "Vale",
     weight: 50,
     category: "power freestyle",
     gender: "femenino"
   },
   {
-    id: "andy",
+    id: "malin",
     name: "Andrea",
     weight: 45,
     category: "power freestyle",
     gender: "femenino"
   },
   {
-    id: "andres",
-    name: "Andres",
+    id: "ginon",
+    name: "Ginon",
     weight: 65,
     category: "power freestyle",
-    gender: "masculino"
+    gender: "femenino"
   },
   // {
   //   id: "alfredo",
